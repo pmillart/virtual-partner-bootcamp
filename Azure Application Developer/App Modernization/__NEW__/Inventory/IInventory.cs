eaac42ba16988afb87c00a420300b18b21525ca1
Alex Mang <amang@keyticket.eu>
2020-05-20 03:11:46 +0300

    new version

---
+++ Azure Application Developer/App Modernization/__NEW__/Inventory/IInventory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ECommerce.Domain.Inventory
+{
+    public interface IInventory
+    {
+        Task<int> AddStockAsync(string itemId);
+        Task<int> RemoveStockAsync(string itemId);
+        Task<bool> IsItemInInventoryAsync(string itemId);
+        Task<bool> CreateItemAsync(InventoryItem item);
+        Task<InventoryItem> GetItemAsync(string itemId);
+    }
+}
